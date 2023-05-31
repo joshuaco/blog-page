85d608533da7c006147231ceec22c5825dbe983f
Joshua <joxuecont@gmail.com>
2023-05-30 21:26:20 -0400

    production deploy

--- src/scripts/utils/humanize.js
+++ src/scripts/utils/humanize.js
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-await import('dayjs/locale/es-us.js');
+import('dayjs/locale/es-us.js');
 
 dayjs.locale('es-us');
 dayjs.extend(relativeTime);
