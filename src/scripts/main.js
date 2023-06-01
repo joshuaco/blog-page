499a50e041c474f51ee811a834e39a562e521679
Joshua <joxuecont@gmail.com>
2023-06-01 16:34:00 -0400

    change: adding class 'extended' & 'reversed' from DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,15 +10,31 @@ const sortArticleByDate = () => {
   const $cards = document.querySelectorAll('.post-list .card');
   // Technique to convert an array-like object to an array
   const $cardsSorted = [...$cards].sort(sortByDate);
-  const $postList = document.querySelector(".post-list");
+  const $postList = document.querySelector('.post-list');
 
-  [...$postList.children].forEach($child => $child.remove());
+  [...$postList.children].forEach(($child) => $child.remove());
   $postList.append(...$cardsSorted);
-}
+};
+
+const addClassToCards = () => {
+  const $postList = document.querySelectorAll('.post-list')[0];
+  const $cards = [...$postList.children];
+  const $extendedCards = $cards.filter((_, index) => (index + 1) % 4 === 0);
+  const $reversedCards = $cards.filter((_, index) => (index + 1) % 8 == 0);
+
+  $extendedCards.forEach(($card) => {
+    $card.classList.add('card-extended');
+  });
+
+  $reversedCards.forEach(($card) => {
+    $card.classList.add('card-reversed');
+  });
+};
 
 const main = () => {
   humanizeArticlesDate();
   sortArticleByDate();
+  addClassToCards();
 };
 
 main();
